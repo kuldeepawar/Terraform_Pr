99b6ea965b6733e6643cace58f52f8fcdae65a9c
Kuldeep Pawar <37316881+kuldeepawar@users.noreply.github.com>
2025-05-06 20:42:18 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,3 @@
-output "websiteendpoint" {
-    value = aws_s3_bucket.bucket1.website_endpoint
-  
+output "website_endpoint" {
+  value = aws_s3_bucket_website_configuration.bucket1.website_endpoint
 }
